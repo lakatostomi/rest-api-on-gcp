db9310da29b457b0a40c222b0232d57bbc926897
Lakatos Tamas <tommy890108@icloud.com>
2023-09-20 20:49:08 +0200

    README.md added

---
+++ README.md
@@ -0,0 +1,28 @@
+This REST API displays statistical data about the population of the countries and areas of World.
+The API is deployed on Google Cloud Platform using its APIs and Services. 
+
+Version 1:
+
+In the first version the app has used a file handler module which reads the data from .json or .xml
+file depends on a property value. The input data is stored in H2 inMemoryDatabase. Only 3 endpoint has been implemented to handle requests:
+ - find all with pagination,
+ - find by Country Code,
+ - and find by Year.
+
+I used Google's JIB plugin to build and push the image to Google Container Registry than I used Cloud Run to deploy the container and the input files were stored in a public bucket on GCP.
+
+Version 2:
+
+The file input was replaced by BigQuery, I have created a dataset from the .json file and the REST API queries the data on startup. H2 inMemoryDatabase was kept on in order to handle pagination easier. I did not remove the file handler remove...
+The GCP infrastructure has changed a lot from the previous one:
+ - custom VPC and Firewall rules
+ - container runs on a VM instance
+ - Managed VM Instance Group is created from instance template
+ - Global HTTP Load Balancer has implemented
+ - Cloud Domain and DNS recordset has been set up
+
+The related Terraform code and GitLab CI pipeline configuration is available on GitLab:
+https://gitlab.com/terraform_projects2/web_app_on_gcp
+
+In this project I was focusing on to successfully set up a GCP infrastructure with Terraform, not the REST API itself! 
+ 
--- rest-webservice/src/main/java/org/example/demo/rest/webservice/InitDataOnStartup.java
+++ rest-webservice/src/main/java/org/example/demo/rest/webservice/InitDataOnStartup.java
@@ -1,41 +1,59 @@
 package org.example.demo.rest.webservice;
 
-import org.example.demo.file.io.dao.FileRepository;
-import org.example.demo.file.io.dto.CountryDTO;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.FieldValueList;
+import com.google.cloud.bigquery.QueryJobConfiguration;
 import org.example.demo.rest.webservice.model.Country;
 import org.example.demo.rest.webservice.repository.CountryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class InitDataOnStartup implements CommandLineRunner, Converter<CountryDTO, Country> {
+public class InitDataOnStartup implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InitDataOnStartup.class);
-    private final FileRepository<CountryDTO> fileRepository;
     private final CountryRepository countryRepository;
+    private final BigQuery bigQuery;
 
-    public InitDataOnStartup(FileRepository<CountryDTO> fileRepository, CountryRepository countryRepository) {
-        this.fileRepository = fileRepository;
+    public InitDataOnStartup(CountryRepository countryRepository, BigQuery bigQuery) {
         this.countryRepository = countryRepository;
+        this.bigQuery = bigQuery;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        LOGGER.info("Initializing data...");
-        List<Country> countryList = fileRepository.readFile().stream()
-                .map(this::convert).toList();
-        countryRepository.saveAllAndFlush(countryList);
-        LOGGER.info("...has finished successfully");
+        LOGGER.info("Initializing data has started...");
+        readDataSet();
+        countryRepository.saveAllAndFlush(readDataSet());
+        LOGGER.info("...data initializing has finished successfully!");
     }
 
-    @Override
-    public Country convert(CountryDTO source) {
-        return new Country(null, source.countryName(), source.countryCode(), Year.of(source.year().getValue()), source.value());
+    public List<Country> readDataSet() throws InterruptedException {
+        String query = "SELECT * FROM country-population-statistics.country_population.statistics";
+        QueryJobConfiguration queryJobConfiguration = QueryJobConfiguration.newBuilder(query).build();
+        return convert(bigQuery.query(queryJobConfiguration).iterateAll());
+    }
+
+
+    public List<Country> convert(Iterable<FieldValueList> source) {
+        List<Country> countryList = new ArrayList<>();
+        for (FieldValueList fieldValueList : source) {
+            Country country = Country.builder()
+                    .countryName(fieldValueList.get(3).getStringValue())
+                    .countryCode(fieldValueList.get(2).getStringValue())
+                    .year(Year.of(Integer.parseInt(fieldValueList.get(1).getStringValue())))
+                    .value(new BigDecimal(fieldValueList.get(0).getStringValue()))
+                    .build();
+            countryList.add(country);
+        }
+        return countryList;
     }
 }
+
